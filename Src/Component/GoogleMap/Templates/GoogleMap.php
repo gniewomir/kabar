09ddb0933067d33ee114f3d616dee20068356eae
Gniewomir Świechowski <gniewomir@idium.no>
2015-09-09 19:50:13 +0200

    Removed Google Map and Mail components

--- Src/Component/GoogleMap/Templates/GoogleMap.php
+++
@@ -1,3 +0,0 @@
-<div class="google-map-wrapper <?php echo esc_attr($cssClass); ?>">
-	<div id="<?php echo esc_attr($id); ?>" class="google-map"></div>
-</div>
--- Src/Component/GoogleMap/Templates/Message.php
+++
@@ -1,3 +0,0 @@
-<div class="google-map-wrapper <?php echo esc_attr($cssClass); ?>">
-	<h2 class="google-map-message"><?php echo esc_html($message); ?></h1>
-</div>
